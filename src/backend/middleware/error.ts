726f6d3de0e6cafb67b18787e06950ce3684d022
Maciej Pędzich <dnalop.me@gmail.com>
2021-02-13 18:37:53 +0100

    Create error handling middleware

---
+++ src/backend/middleware/error.ts
@@ -0,0 +1,26 @@
+import { Request, Response, NextFunction } from 'express';
+import BaseHttpError from '../errors/base-http';
+
+export default function errorMiddleware(
+  error: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  if (
+    process.env.NODE_ENV === 'development' &&
+    !(error instanceof BaseHttpError)
+  ) {
+    console.error(error);
+  }
+
+  let status = 500;
+  let message = 'An unexpected error occurred';
+
+  if (error instanceof BaseHttpError) {
+    status = error.status;
+    message = error.message;
+  }
+
+  return res.status(status).json({ error: message });
+}
