f5bfb14be0a0a4f7756ff0f9c08e62b625e604fa
Maciej Pędzich <dnalop.me@gmail.com>
2021-02-13 18:13:27 +0100

    Add author information

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,28 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>VENM Auth Demo</h1>
+    <p>&copy; Maciej Pędzich {{ currentYear }}</p>
+    <p>
+      Released under
+      <a
+        href="https://opensource.org/licenses/MIT"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        MIT License
+      </a>
+    </p>
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+export default defineComponent({
+  name: 'About',
+  setup() {
+    const currentYear = ref<number>(new Date().getFullYear());
+
+    return { currentYear };
+  }
+});
+</script>
