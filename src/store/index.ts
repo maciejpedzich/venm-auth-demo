49b62cf828c4c25ae2ef48f65df935f00956c941
Maciej Pędzich <dnalop.me@gmail.com>
2021-02-14 14:38:01 +0100

    Use created-persistedstate plugin in Vuex

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,11 @@
 import { createStore } from 'vuex';
+import createPersistedState from 'vuex-persistedstate';
 
 import auth from './modules/auth';
 
 export default createStore({
   modules: {
     auth
-  }
+  },
+  plugins: [createPersistedState()]
 });
