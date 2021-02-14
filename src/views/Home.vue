4f74239e4baa9ed16ba77e1ddb667cbca51cc6de
Maciej Pędzich <dnalop.me@gmail.com>
2021-02-14 17:03:18 +0100

    Add message for authenticated user

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,29 @@
 <template>
-  <h1>Welcome Home!</h1>
+  <div v-if="currentUser">
+    <h1>Welcome home, {{ currentUser.username }}!</h1>
+    <p>You can see this page, because you're logged in.</p>
+  </div>
+  <div v-else>
+    <p>What? You're not supposed to be here. I don't even know you...</p>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, computed } from 'vue';
+import { useStore } from 'vuex';
+
+import UserPayload from '../interfaces/payloads/user';
 
 export default defineComponent({
-  name: 'Home'
+  name: 'Home',
+  setup() {
+    const store = useStore();
+
+    const currentUser = computed<UserPayload | null>(
+      () => store.getters.getCurrentUser
+    );
+
+    return { currentUser };
+  }
 });
 </script>
